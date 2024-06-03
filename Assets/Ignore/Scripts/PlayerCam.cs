edc7e29956d1407009a56419c9f2b14913b7e32a
duckasick <ditispepijn@live.nl>
2024-06-03 18:40:32 +0200

    fasdafsdfasdasdf

--- Assets/Ignore/Scripts/PlayerCam.cs
+++ Assets/Ignore/Scripts/PlayerCam.cs
@@ -24,6 +24,7 @@ public class PlayerCam : MonoBehaviour
     public float maxMovementSpeed;
     public float minFov;
     public float maxFov;
+    private bool fuck;
 
     private void Start()
     {
@@ -33,20 +34,24 @@ public class PlayerCam : MonoBehaviour
 
     private void Update()
     {
-        // get mouse input
-        float mouseX = Input.GetAxisRaw("Mouse X") * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * sensY;
+        if (Input.GetKey(KeyCode.W)) { fuck = true; }
+        if (fuck)
+        {
+            // get mouse input
+            float mouseX = Input.GetAxisRaw("Mouse X") * sensX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * sensY;
 
-        yRotation += mouseX * multiplier;
+            yRotation += mouseX * multiplier;
 
-        xRotation -= mouseY * multiplier;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation -= mouseY * multiplier;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        // rotate cam and orientation
-        camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+            // rotate cam and orientation
+            camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
 
-        if (useFluentFov) HandleFov();
+            if (useFluentFov) HandleFov();
+        }
     }
 
     private void HandleFov()
